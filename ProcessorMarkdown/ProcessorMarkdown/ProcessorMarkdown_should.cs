5b6a7a2b120c632ed7248fc60dd90b51bcc9d366
MashaTelyatnikova <mashatelatnikova@gmail.com>
2014-11-02 16:46:01 +0500

    tests fixed

--- ProcessorMarkdown/ProcessorMarkdown/ProcessorMarkdown_should.cs
+++ ProcessorMarkdown/ProcessorMarkdown/ProcessorMarkdown_should.cs
@@ -10,33 +10,30 @@ namespace ProcessorMarkdown
     [TestFixture]
     public class ProcessorMarkdown_should
     {
+        static void Test(string inputText, string expectedResult)
+        {
+            var processor = new ProcessorMarkdown(inputText);
+            var actualResult = processor.GetResultOfProcessing();
+
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+
         [Test]
         public void return_empty_response_for_empty_input()
         {
-            var processor = new ProcessorMarkdown("");
-            var actualResult = processor.GetResultOfProcessing();
-
-            Assert.That(actualResult, Is.Empty);
+            Test("", "");
         }
 
         [Test]
         public void resturn_one_paragraph_for_simple_line()
         {
-            var processor = new ProcessorMarkdown("Hello, World!");
-            var actualResult = processor.GetResultOfProcessing();
-            var expectedResult = "<p>Hello, World!</p>";
-
-            Assert.That(actualResult, Is.EqualTo(expectedResult));
+            Test("Hello, World!", "<p>Hello, World!</p>");
         }
 
         [Test]
         public void return_few_paragraphs_for_simple_text()
         {
-            var processor = new ProcessorMarkdown("Hello, World!\n\nMy name is Masha.");
-            var actualResult = processor.GetResultOfProcessing();
-            var expectedResult = "<p>Hello, World!</p><p>My name is Masha.</p>";
-
-            Assert.That(actualResult, Is.EqualTo(expectedResult));
+            Test("Hello, World!\n\nMy name is Masha.", "<p>Hello, World!</p><p>My name is Masha.</p>");
         }
     }
 }
