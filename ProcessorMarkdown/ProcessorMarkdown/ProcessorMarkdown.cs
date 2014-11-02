dc0119faae15187b6a19bd7607e88b4de9e18dcd
MashaTelyatnikova <mashatelatnikova@gmail.com>
2014-11-02 17:17:28 +0500

    Добавлен код, делающий тест зеленым(текст с выделенными подчеркивание словами)

--- ProcessorMarkdown/ProcessorMarkdown/ProcessorMarkdown.cs
+++ ProcessorMarkdown/ProcessorMarkdown/ProcessorMarkdown.cs
@@ -18,9 +18,10 @@ namespace ProcessorMarkdown
         public string GetResultOfProcessing()
         {
             if (string.IsNullOrEmpty(textMarkdown)) return string.Empty;
-
-            var paragraphs = Regex.Split(textMarkdown, @"\n\n").ToList();
-
+            
+            
+            var paragraphs = Regex.Split(textMarkdown, @"\n\n").Select(AutomateReplacement.GetNewString);
+            
             return paragraphs.Aggregate("", (current, paragraph) => current + string.Format("<p>{0}</p>", paragraph));
         }
     }
