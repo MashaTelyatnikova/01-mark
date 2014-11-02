191561d8d5a1b281f53da77f1e1f8b2e1a45b02b
MashaTelyatnikova <mashatelatnikova@gmail.com>
2014-11-02 20:13:52 +0500

    Рефакторинг класса процессора (переименования)

--- ProcessorMarkdown/ProcessorMarkdown/ProcessorMarkdown.cs
+++ ProcessorMarkdown/ProcessorMarkdown/ProcessorMarkdown.cs
@@ -10,7 +10,7 @@ namespace ProcessorMarkdown
     public class ProcessorMarkdown
     {
         private readonly string textMarkdown;
-        private readonly Dictionary<string, string> spec = new Dictionary<string, string>()
+        private readonly Dictionary<string, string> htmlRepresentationOfSpecialCharacter = new Dictionary<string, string>()
         {
             {"<", "&lt;"},
             {">", "&gt;"},
@@ -28,14 +28,14 @@ namespace ProcessorMarkdown
             if (string.IsNullOrEmpty(textMarkdown)) return string.Empty;
             
             
-            var paragraphs = Regex.Split(textMarkdown, @"\n\n").Select(ReplaceSpec).Select(ReplaceSelectionsParagraphOnTags);
+            var paragraphs = Regex.Split(textMarkdown, @"\n\n").Select(ReplaceSpecialCharacters).Select(ReplaceSelectionsParagraphOnTags);
             
             return paragraphs.Aggregate("", (current, paragraph) => current + string.Format("<p>{0}</p>", paragraph));
         }
 
-        private string ReplaceSpec(string paragraph)
+        private string ReplaceSpecialCharacters(string paragraph)
         {
-            return spec.Keys.Aggregate(paragraph, (current, v) => current.Replace(v, spec[v]));
+            return htmlRepresentationOfSpecialCharacter.Keys.Aggregate(paragraph, (current, v) => current.Replace(v, htmlRepresentationOfSpecialCharacter[v]));
         }
 
         private static string ReplaceSelectionsParagraphOnTags(string paragraph)
