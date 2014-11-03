52d607c6d93897fe593d66714340c338f208761e
MashaTelyatnikova <mashatelatnikova@gmail.com>
2014-11-03 16:09:42 +0500

    В Main добавлен код, который приписывает к результату процессинга заголовки для нормального отображения в браузерах и для прохождения валидации

--- ProcessorMarkdown/ProcessorMarkdown/Program.cs
+++ ProcessorMarkdown/ProcessorMarkdown/Program.cs
@@ -15,7 +15,12 @@ namespace ProcessorMarkdown
             var processorMarkDown = new ProcessorMarkdown(File.ReadAllText(fileName));
             var result = processorMarkDown.GetResultOfProcessing();
 
-            File.WriteAllText(Path.ChangeExtension(fileName, "html"), result);
+            File.WriteAllText(Path.ChangeExtension(fileName, "html"), GetHtmlTextWithHeaders(result), Encoding.UTF8);
+        }
+
+        private static string GetHtmlTextWithHeaders(string text)
+        {
+            return string.Format("<!DOCTYPE HTML><HTML><HEAD><TITLE> </TITLE><META http-equiv=\"Content-Type\" content=\"text/html; charset=utf8\"></HEAD><BODY>{0}</BODY></HTML>", text);
         }
     }
 }
