ac90d221b9504eac5d89337aef3dc40abf4e6db5
MashaTelyatnikova <mashatelatnikova@gmail.com>
2014-11-03 23:03:22 +0500

    Изменена структура тестов. Добавлены новые красные тесты(на все случаи)

--- ProcessorMarkdown/ProcessorMarkdown/ProcessorMarkdown_should.cs
+++ ProcessorMarkdown/ProcessorMarkdown/ProcessorMarkdown_should.cs
@@ -13,7 +13,7 @@ namespace ProcessorMarkdown
         static void Test(string inputText, string expectedResult)
         {
             var processor = new ProcessorMarkdown(inputText);
-            var actualResult = processor.GetResultOfProcessing();
+            var actualResult = processor.GetHtmlResultOfProcessing();
 
             Assert.That(actualResult, Is.EqualTo(expectedResult));
         }
@@ -70,6 +70,66 @@ namespace ProcessorMarkdown
         public void test()
         {
             Test("My _name \\_is\\_ Masha.", "<p>My _name _is_ Masha.</p>");
+        }
+
+        [Test]
+        public void test1()
+        {
+            Test("Hello, `world _my name is_ hahaha` _my code_", "<p>Hello, <code>world _my name is_ hahaha</code> <em>my code</em></p>");
+        }
+
+        [Test]
+        public void test2()
+        {
+            Test("Hello, `world _my name is_ hahaha` _my 'code_", "<p>Hello, <code>world _my name is_ hahaha</code> <em>my 'code</em></p>");
+        }
+
+        [Test]
+        public void test3()
+        {
+            Test("Hello, world__12_3 _www_!", "<p>Hello, world__12_3 <em>www</em>!</p>");
+        }
+
+        [Test]
+        public void test4()
+        {
+            Test("_Hello __ddd kl__ , __kkk__ _ world__12_3 _www_!", "<p><em>Hello <strong>ddd kl</strong> , <strong>kkk</strong> </em> world__12_3 <em>www</em>!</p>");
+        }
+
+        [Test]
+        public void test5()
+        {
+            Test("__непарные _символы не считаются `выделением.", "<p>__непарные _символы не считаются `выделением.</p>");
+        }
+
+        [Test]
+        public void return_text_with_replaced_special_characters()
+        {
+            Test("Hello <p> Masha </p> \"Hi\" ©", "<p>Hello &lt;p&gt; Masha &lt;/p&gt; &quot;Hi&quot; &copy;</p>");
+        }
+
+        [Test]
+        public void return_correct_sections_with_code()
+        {
+            Test("Hello, my name is `Masha _Telyatnikova_ hfhfh`.", "<p>Hello, my name is <code>Masha _Telyatnikova_ hfhfh</code>.</p>");
+        }
+
+        [Test]
+        public void ignore_unpaired_quote()
+        {
+            Test("Hello, my name is `Masha _Telyatnikova_ hfhfh.", "<p>Hello, my name is `Masha <em>Telyatnikova</em> hfhfh.</p>");
+        }
+
+        [Test]
+        public void correctly_replace_escape()
+        {
+            Test("Hello, my name \\_Masha \\r", "<p>Hello, my name _Masha r</p>");
+        }
+
+        [Test]
+        public void replace_double_underline_on_tag_strong()
+        {
+            Test("Hello, my name_dj __Masha dd__ Hi_\n\n_msmsm __hello__ mmm_", "<p>Hello, my name_dj <strong>Masha dd</strong> Hi_</p><p><em>msmsm <strong>hello</strong> mmm</em></p>");
         }
     }
 }
